eb8fe0419924b1fba5bb540b3b6dced4090e700b
OleksiiP <alex1204park@gmail.com>
2024-05-11 12:35:13 +0300

    task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { Header } from 'components';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchBaseCurrency } from './redux/currency/operations';
+import { setBaseCurrency } from './redux/currency/currencySlice';
 
 const Home = lazy(() => import('pages/Home'));
 const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    const success = ({ coords }) => {
+      dispatch(fetchBaseCurrency(coords));
+    };
+
+    const error = () => {
+      dispatch(setBaseCurrency('USD'));
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Header />}>
